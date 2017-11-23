230bf4a14cd842f10d810ba5c1ef01b8f60c3ae8
Pankaja Ambalgi <pankaja.ambalgi@student.fh-kiel.de>
2017-11-23 10:33:56 +0100

    Important variables

--- snakeGame.py
+++ snakeGame.py
@@ -12,7 +12,25 @@ else:
     print ("(+) PyGame initialized successfully!!")
 
 # play surface
-
 play_surface = pygame.display.set_mode((720,460))
 pygame.display.set_caption('Snake_Game !!')
-time.sleep(5)
+
+# Colors
+red = pygame.Color(255, 0, 0) #gameover
+green = pygame.Color(0, 255, 0) #snake
+#blue = pygame.Color(0, 0, 255) #food
+white = pygame.Color(255, 255, 255) #background
+black = pygame.Color(0, 0, 0) #score
+brown = pygame.Color(165, 42, 42) #food
+
+#FPS Controller - Frame per second controller
+fpsController = pygame.time.Clock()
+
+snakePos = [100,50] #initial position
+snakeBody = [[100,50],[90,50],[80,50]]
+
+foodPos = [random.randrange(1,72)*10, random.randrange(1,46)*10]
+foodSpawn = True
+
+direction = 'RIGHT'
+changeTo = direction
