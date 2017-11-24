5cda2b2e3d2d207c7e3207e2204f2e348c89cde2
Pankaja Ambalgi <pankaja.ambalgi@student.fh-kiel.de>
2017-11-24 17:16:44 +0100

    Drawing snake body

--- snakeGame.py
+++ snakeGame.py
@@ -44,55 +44,65 @@ def gameOver():
     playSurface.blit(GOSurf, GOrect)
     pygame.display.flip()
 
-gameOver()
-time.sleep(10)
-pygame.quit() #pygame quit
-sys.exit() #console exit
+    time.sleep(10)
+    pygame.quit() #pygame quit
+    sys.exit() #console exit
 
 # Main Logic of the game
-import pygame
 while True:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT():
+        if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            if event.type == pygame.K_RIGHT or event.key == ord('d'):
+            if event.key == pygame.K_RIGHT or event.key == ord('d'):
                 changeTo = 'RIGHT'
-            if event.type == pygame.K_LEFT or event.key == ord('a'):
+            if event.key == pygame.K_LEFT or event.key == ord('a'):
                 changeTo = 'LEFT'
-            if event.type == pygame.K_UP or event.key == ord('w'):
+            if event.key == pygame.K_UP or event.key == ord('w'):
                 changeTo = 'UP'
-            if event.type == pygame.K_DOWN or event.key == ord('s'):
+            if event.key == pygame.K_DOWN or event.key == ord('s'):
                 changeTo = 'DOWN'
-            if event.type == pygame.K_ESCAPE:
-                pygame.event.post(pygame.event.Event(QUIT))
+            if event.key == pygame.K_ESCAPE:
+                pygame.event.post(pygame.event.Event(pygame.QUIT))
 
 # validation of direction
-            if changeTo == 'LEFT' and not direction == 'RIGHT':
-                direction = 'LEFT'
-            if changeTo == 'RIGHT' and not direction == 'LEFT':
-                direction = 'RIGHT'
-            if changeTo == 'UP' and not direction == 'DOWN':
-                direction = 'UP'
-            if changeTo == 'DOWN' and not direction == 'UP':
-                direction = 'DOWN'
+    if changeTo == 'LEFT' and not direction == 'RIGHT':
+        direction = 'LEFT'
+    if changeTo == 'RIGHT' and not direction == 'LEFT':
+        direction = 'RIGHT'
+    if changeTo == 'UP' and not direction == 'DOWN':
+        direction = 'UP'
+    if changeTo == 'DOWN' and not direction == 'UP':
+        direction = 'DOWN'
 
 #changing direction update snake position
-            if direction == 'RIGHT':
-                snakePos[0] += 10
-            if direction == 'LEFT':
-                snakePos[0] -= 10
-            if direction == 'UP':
-                snakePos[1] += 10
-            if direction == 'DOWN':
-                snakePos[1] -= 10
+    if direction == 'RIGHT':
+        snakePos[0] += 10
+    if direction == 'LEFT':
+        snakePos[0] -= 10
+    if direction == 'UP':
+        snakePos[1] -= 10
+    if direction == 'DOWN':
+        snakePos[1] += 10
 
-            snakeBody.insert(0, list(snakePos))
-            if snakePos[0] == foodPos[0] and snakePos[1] == foodPos[1]:
-                foodSpawn = False
-            else:
-                snakeBody.pop()
-            if foodSpawn == False:
-                foodPos = [random.randrange(1,72)*10, random.randrange(1,46)*10]
-                foodSpawn = True
+    snakeBody.insert(0, list(snakePos))
+    if snakePos[0] == foodPos[0] and snakePos[1] == foodPos[1]:
+        foodSpawn = False
+    else:
+        snakeBody.pop()
+    #Food Spawn
+    if foodSpawn == False:
+        foodPos = [random.randrange(1,72)*10, random.randrange(1,46)*10]
+    foodSpawn = True
+
+    # Background fill
+    playSurface.fill(white)
+
+    #Draw Snake
+    for pos in snakeBody:
+        pygame.draw.rect(playSurface, green, pygame.Rect(pos[0],pos[1],10,10))
+
+    pygame.draw.rect(playSurface, brown, pygame.Rect(foodPos[0], foodPos[1], 10, 10))
+    pygame.display.flip()
+    fpsController.tick(25)
