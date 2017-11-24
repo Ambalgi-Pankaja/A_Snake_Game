abe78ace23fca314718510f9719ab647e3e76250
Pankaja Ambalgi <pankaja.ambalgi@student.fh-kiel.de>
2017-11-24 15:56:13 +0100

    adding direction and snake body mechanism

--- snakeGame.py
+++ snakeGame.py
@@ -12,7 +12,7 @@ else:
     print ("(+) PyGame initialized successfully!!")
 
 # play surface
-play_surface = pygame.display.set_mode((720,460))
+playSurface = pygame.display.set_mode((720,460))
 pygame.display.set_caption('Snake_Game !!')
 
 # Colors
@@ -34,3 +34,65 @@ foodSpawn = True
 
 direction = 'RIGHT'
 changeTo = direction
+
+# Game over function
+def gameOver():
+    myFont = pygame.font.SysFont('calibri', 72)
+    GOSurf = myFont.render('Game Over!', True, red)
+    GOrect = GOSurf.get_rect()
+    GOrect.midtop = (360, 15)
+    playSurface.blit(GOSurf, GOrect)
+    pygame.display.flip()
+
+gameOver()
+time.sleep(10)
+pygame.quit() #pygame quit
+sys.exit() #console exit
+
+# Main Logic of the game
+import pygame
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT():
+            pygame.quit()
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.type == pygame.K_RIGHT or event.key == ord('d'):
+                changeTo = 'RIGHT'
+            if event.type == pygame.K_LEFT or event.key == ord('a'):
+                changeTo = 'LEFT'
+            if event.type == pygame.K_UP or event.key == ord('w'):
+                changeTo = 'UP'
+            if event.type == pygame.K_DOWN or event.key == ord('s'):
+                changeTo = 'DOWN'
+            if event.type == pygame.K_ESCAPE:
+                pygame.event.post(pygame.event.Event(QUIT))
+
+# validation of direction
+            if changeTo == 'LEFT' and not direction == 'RIGHT':
+                direction = 'LEFT'
+            if changeTo == 'RIGHT' and not direction == 'LEFT':
+                direction = 'RIGHT'
+            if changeTo == 'UP' and not direction == 'DOWN':
+                direction = 'UP'
+            if changeTo == 'DOWN' and not direction == 'UP':
+                direction = 'DOWN'
+
+#changing direction update snake position
+            if direction == 'RIGHT':
+                snakePos[0] += 10
+            if direction == 'LEFT':
+                snakePos[0] -= 10
+            if direction == 'UP':
+                snakePos[1] += 10
+            if direction == 'DOWN':
+                snakePos[1] -= 10
+
+            snakeBody.insert(0, list(snakePos))
+            if snakePos[0] == foodPos[0] and snakePos[1] == foodPos[1]:
+                foodSpawn = False
+            else:
+                snakeBody.pop()
+            if foodSpawn == False:
+                foodPos = [random.randrange(1,72)*10, random.randrange(1,46)*10]
+                foodSpawn = True
